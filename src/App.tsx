974bab3b66fc372523d5a3db808cd7527e23b65f
Vaibhav Domadia <vaibhavdomadia.code@gmail.com>
2023-09-17 11:39:05 +0530

    Adding base react-flow layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
 import React from 'react';
+import ReactFlow, { Background, BackgroundVariant } from 'reactflow';
+
+import 'reactflow/dist/style.css';
+
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    return (
+        <div style={{width: '100vw', height: '100vh'}}>
+            <ReactFlow>
+                <Background variant={BackgroundVariant.Dots} gap={12} size={1} style={{backgroundColor: "#202020"}}/>
+            </ReactFlow>
+        </div>
+    )
 }
-
-export default App;
